e80b495584d9c2d5c5bc29ab738ca1f099c6e033
minhtamos <tam310599@gmail.com>
2020-03-08 23:52:53 +0700

    FuncTable props, functional

--- client/src/components/Admin/Content/AdminFuncTable.tsx
+++ client/src/components/Admin/Content/AdminFuncTable.tsx
@@ -1,40 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { AdminTable } from "../Content/AdminTable";
 import { AdminButton } from "../Content/AdminButton";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
+interface col {
+  name: string;
+  selector: string;
+  sortable: boolean;
+}
 interface props {
-    
+  tableName: string;
+  tableTitle: string;
+  columns: Array<col>;
 }
 
-export const CategoryTable = () => {
-  const columns = [
-    {
-      name: "ID",
-      selector: "id",
-      sortable: true
-    },
-    {
-      name: "Name",
-      selector: "name",
-      sortable: true
-    },
-    {
-      name: "Created At",
-      selector: "createdAt",
-      sortable: true
-    },
-    {
-      name: "Updated At",
-      selector: "updatedAt",
-      sortable: true
-    }
-  ];
+export const CategoryTable = ({ tableName, tableTitle, columns }: props) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const getTableData = () => {
-    fetch("/api/category/all").then(res => {
+    fetch(`/api/${tableName}/all`).then(res => {
       res.json().then(data => {
+        switch (1) {
+          default:
+            break;
+        }
         const { categories } = data;
         setData(categories);
         setLoading(false);
@@ -45,16 +34,16 @@ export const CategoryTable = () => {
     Swal.fire({
       title: `<p class="text-admin-title">Are you sure?</p>`,
       html: `<p class="text-admin-title">You gonna delete row id ${row.id}!</p>`,
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#BA3632',
-      cancelButtonColor: '#2799E9',
-      confirmButtonText: 'Yes, delete it!',
-      background: '#1E2A31',
-    }).then(async (result) => {
+      confirmButtonColor: "#BA3632",
+      cancelButtonColor: "#2799E9",
+      confirmButtonText: "Yes, delete it!",
+      background: "#1E2A31"
+    }).then(async result => {
       if (result.value) {
         const body = { id: row.id };
-        const res = await fetch("/api/category", {
+        const res = await fetch(`/api/${tableName}`, {
           method: "DELETE",
           mode: "cors",
           headers: {
@@ -67,31 +56,38 @@ export const CategoryTable = () => {
           Swal.fire({
             title: '<p class="text-admin-title">DELETED</p>',
             html: `<p class="text-admin-title">${status}</p>`,
-            background: '#1E2A31',
-            confirmButtonColor: '#2799E9'
-          })
+            background: "#1E2A31",
+            confirmButtonColor: "#2799E9"
+          });
           getTableData();
         } else {
           const { errors } = await res.json();
           Swal.fire({
             title: '<p class="text-admin-title">ERROR</p>',
             html: `<p class="text-admin-title">${errors[0].message}</p>`,
-            background: '#1E2A31',
-            confirmButtonColor: '#2799E9'
-          })
+            background: "#1E2A31",
+            confirmButtonColor: "#2799E9"
+          });
         }
       }
-    })
-  }
+    });
+  };
   useEffect(() => {
     getTableData();
   }, []);
   return loading ? (
-    <div>loading</div>
+    <div>loading table...</div>
   ) : (
     <div>
-      <p className="text-admin-word">"Double click the row to delete that row"</p>
-      <AdminTable title="Categories" data={data} columns={columns} onRowDoubleClicked={deleteTableRow}/>
+      <p className="text-admin-word">
+        "Double click the row to delete that row"
+      </p>
+      <AdminTable
+        title="Categories"
+        data={data}
+        columns={columns}
+        onRowDoubleClicked={deleteTableRow}
+      />
       <AdminButton title="Reload" onClick={getTableData} />
     </div>
   );
