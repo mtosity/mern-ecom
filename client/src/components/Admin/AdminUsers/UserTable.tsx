418580a23cc58accdbcb5e67730e2ede3ff21bdc
minhtamos <tam310599@gmail.com>
2020-03-12 21:27:40 +0700

    update overflow user table

---
+++ client/src/components/Admin/AdminUsers/UserTable.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { AdminFuncTable } from "../Content/AdminFuncTable";
+
+export const UserTable = () => {
+  const columns = [
+    {
+      name: "Email",
+      selector: "email",
+      sortable: true,
+      allowOverflow: true,
+    },
+    {
+      name: "Role",
+      selector: "role",
+      sortable: true,
+      allowOverflow: true,
+    },
+    {
+      name: "Name",
+      selector: "name",
+      sortable: true,
+      allowOverflow: true,
+    },
+    {
+      name: "Address",
+      selector: "address",
+      sortable: true,
+      allowOverflow: true,
+    },
+    {
+      name: "Avatar URL",
+      selector: "avatar",
+      sortable: true,
+      allowOverflow: true,
+    },
+  ];
+  return (
+    <AdminFuncTable columns={columns} tableName="user" tableTitle="User"/>
+  )
+}
