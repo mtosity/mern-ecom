198df3f3d5f9bfe0675c7f7758a6ec66bcf52ceb
minhtamos <tam310599@gmail.com>
2020-03-18 16:47:13 +0700

    resize icon to responsive

--- client/src/components/TopNav.tsx
+++ client/src/components/TopNav.tsx
@@ -1,39 +1,43 @@
 import React from "react";
 import Logo from "../images/trade.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown, faStoreAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronDown,
+  faStoreAlt,
+  faCaretDown
+} from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 
 export const TopNav = () => {
   return (
     <div
-      className="w-full flex items-center px-16 justify-between"
+      className="w-full flex items-center px-16 justify-between md:px-8"
       style={{ height: "82px" }}
     >
       {/* LOGO */}
       <div className="flex items-center">
         <div className="flex items-center">
-          <FontAwesomeIcon icon={faStoreAlt} size='2x' color="red" />
+          <FontAwesomeIcon icon={faStoreAlt} size="2x" color="red" />
           <p className="text-xl text-red-600 font-bold ml-2">MTOS</p>
           <p className="text-lg text-red-400">hop</p>
         </div>
-        <div className="flex ml-4 items-center">
+        <div className="flex ml-4 items-center lg:hidden">
           <a
             href="#"
-            className="inline-block px-8 py-3 hover:bg-red-500 hover:text-white text-gray-700"
+            className="inline-block px-4 ml-4 py-3 hover:bg-red-500 hover:text-white text-gray-700"
           >
             HOME
           </a>
           <a
             href="#"
-            className="inline-block px-8 py-3 hover:bg-red-500 hover:text-white text-gray-700"
+            className="inline-block px-4 ml-4 py-3 hover:bg-red-500 hover:text-white text-gray-700"
           >
             ABOUT
           </a>
           <div className="group">
             <a
               href="#"
-              className="flex items-center hover:bg-red-500 hover:text-white px-8 py-3"
+              className="flex items-center hover:bg-red-500 hover:text-white text-gray-700 px-4 ml-4 py-3"
             >
               CATALOG
               <FontAwesomeIcon icon={faChevronDown} className="h-3 w-3 ml-2" />
@@ -54,23 +58,32 @@ export const TopNav = () => {
           </div>
           <a
             href="#"
-            className="inline-block px-8 py-3 hover:bg-red-500 hover:text-white text-gray-700"
+            className="inline-block px-4 ml-4 py-3 hover:bg-red-500 hover:text-white text-gray-700"
           >
             BLOG
             <FontAwesomeIcon icon={faChevronDown} className="h-3 w-3 ml-2" />
           </a>
           <a
             href="#"
-            className="inline-block px-8 py-3 hover:bg-red-500 hover:text-white text-gray-700"
+            className="inline-block px-4 ml-4 py-3 hover:bg-red-500 hover:text-white text-gray-700"
           >
             CONTACT
           </a>
         </div>
       </div>
-      <div className="py-2 px-8 flex items-center border border-red-400 rounded">
-        <FontAwesomeIcon icon={faUser} color="red" />
-        <p className="ml-2 text-red-600">Account</p>
-        <FontAwesomeIcon icon={faChevronDown} className="ml-2" color="red" />
+      <div className="flex">
+        <div className="py-2 px-8 flex items-center border border-red-400 rounded">
+          <FontAwesomeIcon icon={faUser} color="red" />
+          <p className="ml-2 text-red-600 md:hidden">Account</p>
+          <FontAwesomeIcon
+            icon={faChevronDown}
+            className="ml-2 md:hidden"
+            color="red"
+          />
+        </div>
+        <div className="py-2 px-8 flex items-center border border-red-400 rounded hidden lg:inline-block ml-4">
+          <FontAwesomeIcon icon={faCaretDown} color="red" />
+        </div>
       </div>
     </div>
   );
