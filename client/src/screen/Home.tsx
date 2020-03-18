8624617581b6a322748f44f2071ed9d00e694858
minhtamos <tam310599@gmail.com>
2020-03-18 16:47:37 +0700

    add feature cate and title to home

--- client/src/screen/Home.tsx
+++ client/src/screen/Home.tsx
@@ -1,24 +1,18 @@
-import React, { } from "react";
+import React from "react";
 
-import {Banner} from "../components/Banner";
-import {SideCate} from "../components/SideCate";
+import { Banner } from "../components/Banner";
+import { SideCate } from "../components/SideCate";
 import { TopInfo } from "../components/TopInfo";
+import { FeatureCategories } from "../components/FeatureCategories";
+import { MiddleTitle } from "../components/MiddleTitle";
 
 export const Home = () => {
   return (
-    <div className="w-full">
-      {/* navbar */}
-      {/* banner and categories */}
-      <a
-        href=""
-        className="w-full text-center text-blue-400 bg-gray-800 p-2 md:inline-block md:mt-2 hidden"
-      >
-        Categories
-      </a>
-      <div className="w-full flex">
-        <div className="shadow-md">
-          <Banner />
-        </div>
+    <div className="w-full flex">
+      <div className="shadow-md">
+        <Banner />
+        <FeatureCategories/>
+        <MiddleTitle/>
       </div>
     </div>
   );
