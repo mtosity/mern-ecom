63fc0195aacbbfd546d993cdaaac167af747d31b
minhtamos <tam310599@gmail.com>
2020-03-18 16:46:53 +0700

    middle title

---
+++ client/src/components/MiddleTitle.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const MiddleTitle = () => {
+  return (
+    <div className="w-full flex justify-center items-center flex-col">
+      <h1 className="jo-font font-extrabold text-4xl">New Arrivals</h1>
+      <div className="h-20 border-t-2 border-red-600 w-20"></div>
+    </div>
+  );
+};
