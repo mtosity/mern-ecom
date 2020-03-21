12f5a93e44fd78d3fbd2f387550e77cefe81598f
minhtamos <tam310599@gmail.com>
2020-03-21 21:25:14 +0700

    icon animation

--- client/src/components/Advantages.tsx
+++ client/src/components/Advantages.tsx
@@ -1,21 +1,39 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlaneDeparture, faHeadphonesAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPlaneDeparture,
+  faHeadphonesAlt
+} from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 export const Advantages = () => {
   return (
     <div className="w-full flex justify-around py-16">
-      <div className="w-1/4 border border-gray-400 p-4 flex items-center">
-        <FontAwesomeIcon icon={faPlaneDeparture} color="red" size="2x"/>
+      <div className="w-1/4 border border-gray-400 p-4 flex items-center group overflow-hidden">
+        <FontAwesomeIcon
+          icon={faPlaneDeparture}
+          color="red"
+          size="2x"
+          className="transform duration-500 group-hover:-translate-y-8 group-hover:translate-x-8"
+        />
         <p className="ml-4">Free Shipping Worldwide</p>
       </div>
-      <div className="w-1/4 border border-gray-400 p-4 flex items-center">
-        <FontAwesomeIcon icon={faHeadphonesAlt} color="red" size="2x"/>
+      <div className="w-1/4 border border-gray-400 p-4 flex items-center group overflow-hidden">
+        <FontAwesomeIcon
+          icon={faHeadphonesAlt}
+          color="red"
+          size="2x"
+          className="transform duration-500 group-hover:rotate-45"
+        />
         <p className="ml-4">24/7 Customer Service</p>
       </div>
-      <div className="w-1/4 border border-gray-400 p-4 flex items-center">
-        <FontAwesomeIcon icon={faHeart} color="red" size="2x"/>
+      <div className="w-1/4 border border-gray-400 p-4 flex items-center group overflow-hidden">
+        <FontAwesomeIcon
+          icon={faHeart}
+          color="red"
+          size="2x"
+          className="transform duration-500 group-hover:rotate-45"
+        />
         <p className="ml-4">Easy Return Policy</p>
       </div>
     </div>
