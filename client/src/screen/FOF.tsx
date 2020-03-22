f43cea97285630f61df4d12a5800d020852b3a21
minhtamos <tam310599@gmail.com>
2020-03-22 16:20:47 -0700

    change 404 picture

--- client/src/screen/FOF.tsx
+++ client/src/screen/FOF.tsx
@@ -5,7 +5,7 @@ import FOFImage from '../images/404.png';
 export const FOF = () => {
     return (
         <div className="flex w-full justify-center">
-            <img src={FOFImage} alt="" className="block"/>
+            <img src="https://www.elsetge.cat/myimg/f/99-992071_404-page-error-with-funny-figure-skull.jpg" alt="" className="block"/>
         </div>
     )
 }
