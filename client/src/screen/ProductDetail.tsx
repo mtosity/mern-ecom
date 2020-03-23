4c6770c30030b62e7739c812a077e9b2c605dccb
minhtamos <tam310599@gmail.com>
2020-03-24 00:24:54 +0700

    add them to product detail

--- client/src/screen/ProductDetail.tsx
+++ client/src/screen/ProductDetail.tsx
@@ -2,31 +2,47 @@ import React from "react";
 import { ProductCarousel } from "../components/ProductDetail/ProductCarousel";
 import { ProductSectionTitle } from "../components/ProductDetail/ProductSectionTitle";
 import { ProductPrice } from "../components/ProductDetail/ProductPrice";
-import { ProductStarts } from "../components/ProductDetail/ProductStarts";
+import { ProductStars } from "../components/ProductDetail/ProductStars";
 import { ProductSizes } from "../components/ProductDetail/ProductSizes";
 import { ProductColors } from "../components/ProductDetail/ProductColors";
 import { AddToCartBtn } from "../components/ProductDetail/AddToCartBtn";
+import { ShareBtns } from "../components/ProductDetail/ShareBtns";
+import { ProductInfo } from "../components/ProductDetail/ProductInfo";
 
 export const ProductDetail = () => {
   return (
-    <div className="w-full flex px-16">
-      <div style={{ flex: 1 }}>
-        <ProductCarousel />
-      </div>
-      <div style={{ flex: 3 }} className="ml-8">
-        <p className="text-4xl jo-font font-extrabold tracking-wide">Proin Lectus Ipsum</p>
-        <div className="flex justify-between">
-            <ProductPrice price={120}/>
-            <ProductStarts stars={4.5}/>
+    <div>
+      <div className="w-full flex px-16 pb-16">
+        <div style={{ flex: 1 }}>
+          <ProductCarousel />
         </div>
-        <ProductSectionTitle title="Quick Overview"/>
-        <p className="">There are many variations of passages of Lorem Ipsum avaable, b majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. make an ami jani nab majority have suffered alteration in some form, variations of passages Lorem Ipsum avaable, b majority</p>
-        <ProductSectionTitle title="Select Size"/>
-        <ProductSizes/>
-        <ProductSectionTitle title="Select Color"/>
-        <ProductColors/>
-        <AddToCartBtn/>
+        <div style={{ flex: 3 }} className="ml-8">
+          <p className="text-4xl jo-font font-extrabold tracking-wide">
+            Proin Lectus Ipsum
+          </p>
+          <div className="flex justify-between">
+            <ProductPrice price={120} />
+            <ProductStars stars={4.5} />
+          </div>
+          <ProductSectionTitle title="Quick Overview" />
+          <p className="">
+            There are many variations of passages of Lorem Ipsum avaable, b
+            majority have suffered alteration in some form, by injected humour,
+            or randomised words which don't look even slightly believable. make
+            an ami jani nab majority have suffered alteration in some form,
+            variations of passages Lorem Ipsum avaable, b majority
+          </p>
+          <ProductSectionTitle title="Select Size" />
+          <ProductSizes />
+          <ProductSectionTitle title="Select Color" />
+          <ProductColors />
+          <div className="flex justify-between items-center mt-8">
+            <AddToCartBtn />
+            <ShareBtns />
+          </div>
+        </div>
       </div>
+      <ProductInfo />
     </div>
   );
 };
