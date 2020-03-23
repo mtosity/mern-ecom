ed1d5883d86a43f0244ff8b50bce15b1e322b613
minhtamos <tam310599@gmail.com>
2020-03-23 22:48:26 +0700

    change from flex to grid

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -1,21 +1,19 @@
 import React from "react";
-import { ProductCard } from "./Product/ProductCard";
+import { ProductCard } from "./ProductCard/ProductCard";
 
 export const ProductList = () => {
   return (
-    <div>
-      <div className="w-full flex justify-between px-16 pb-16">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
-      <div className="w-full flex justify-between px-16 pb-16">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
+    <div className="w-full grid row-gap-8	px-16 pb-16 md:px-8 sm:px-4
+    grid-cols-4 lg:grid-cols-3 md:grid-cols-2
+    ">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
     </div>
   );
 };
