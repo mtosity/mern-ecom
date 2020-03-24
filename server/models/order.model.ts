25b263fa7a295186cae57a8613fc4428110e83f2
minhtamos <tam310599@gmail.com>
2020-03-24 18:08:44 +0700

    order and cart into one table

--- server/models/order.model.ts
+++ server/models/order.model.ts
@@ -30,7 +30,38 @@ Order.init(
       primaryKey: true
     },
     quantity: {
-      type: DataTypes.INTEGER.UNSIGNED
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: {
+          args: [["black", "green", "blue", "red"]],
+          msg: "Order status must be black green blue red"
+        }
+      }
+    },
+    size: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: {
+          args: [["sm", "md", "lg", "xl"]],
+          msg: "Order status must be sm md lg xl"
+        }
+      }
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: {
+          args: [["cart", "ordered", "delivered"]],
+          msg: "Order status must be cart ordered delivered"
+        }
+      }
     }
   },
   {
