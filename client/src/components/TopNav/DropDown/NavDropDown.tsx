347b69d3c7e9f8eb1d0a3c5b1143cd7fb8d5a6ec
minhtamos <tam310599@gmail.com>
2020-03-25 16:08:43 +0700

    change height of transparent space

--- client/src/components/TopNav/DropDown/NavDropDown.tsx
+++ client/src/components/TopNav/DropDown/NavDropDown.tsx
@@ -11,7 +11,7 @@ export const NavDropDown = ({ children }: props) => {
         transform duration-300 transition-all translate-y-8 ease-in-out pointer-events-none
         group-hover:transform group-hover:translate-y-0 group-hover:opacity-100 group-hover:pointer-events-auto"
       >
-        <div className="h-4 opacity-0"></div>
+        <div className="h-2 opacity-0"></div>
         <div className="border-t border-red-400 bg-gray-100 px-12 py-4 shadow-lg rounded-sm">
           {children}
         </div>
