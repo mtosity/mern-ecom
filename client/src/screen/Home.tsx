a6bbcfaf8e4d5985c520f24eb80600ca85d38143
minhtamos <tam310599@gmail.com>
2020-03-26 22:49:39 +0700

    no fetch categories in home

--- client/src/screen/Home.tsx
+++ client/src/screen/Home.tsx
@@ -8,18 +8,8 @@ import { MiddleBanner } from "../components/MiddleBanner";
 import { ProductSlider } from "../components/ProductSlider";
 import { Reviews } from "../components/Reviews";
 import { Advantages } from "../components/Advantages";
-import { useDispatch } from "react-redux";
-import { CategoriesActionType } from "../Actions";
 
 export const Home = () => {
-  const dispatcher = useDispatch();
-  useEffect(() => {
-    fetch("/api/category").then(res => {
-      res.json().then(cate => {
-        dispatcher({type: CategoriesActionType.AddCategory, payload: cate})
-      })
-    })
-  }, [])
   return (
     <div className="w-full">
         <Banner />
