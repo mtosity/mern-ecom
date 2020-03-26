d39b07e0622d7d7938ec2afbe8eccb90e55e63d8
minhtamos <tam310599@gmail.com>
2020-03-26 19:23:05 +0700

    add cate reducer to app reducer

--- client/src/Reducers/CombinedReducers.tsx
+++ client/src/Reducers/CombinedReducers.tsx
@@ -1,15 +1,18 @@
 import { combineReducers, Reducer } from "redux";
 import { AccountReducer, AccountStateInterface } from "./AccountReducer";
 import { GlobalReducer, GlobalStateInterface } from "./GlobalReducer";
+import { CategoriesReducer, CategoriesStateInterface } from "./CategoriesReducer";
 
 export interface ApplicationState {
   GlobalReducer: GlobalStateInterface;
   AccountReducer: AccountStateInterface;
+  CategoriesReducer: CategoriesStateInterface;
 }
 
 export const CombinedReducers: Reducer<ApplicationState> = combineReducers<
   ApplicationState
 >({
   AccountReducer,
-  GlobalReducer
+  GlobalReducer,
+  CategoriesReducer
 });
