f9321659a50841b945d7f3be1ef54fc7565a5738
minhtamos <tam310599@gmail.com>
2020-03-26 22:48:25 +0700

    global loading state

--- client/src/Reducers/GlobalReducer.tsx
+++ client/src/Reducers/GlobalReducer.tsx
@@ -4,17 +4,19 @@ import { GlobalActionType } from "../Actions";
 export interface GlobalStateInterface {
   OpenLoginModal: boolean;
   authenticated: boolean;
+  loading: boolean;
 }
 
 const INIT_STATE: GlobalStateInterface = {
   OpenLoginModal: false,
-  authenticated: false
+  authenticated: false,
+  loading: true
 };
 
-export const GlobalReducer: Reducer<
-GlobalStateInterface,
-  Action
-> = (state = INIT_STATE, action: Action) => {
+export const GlobalReducer: Reducer<GlobalStateInterface, Action> = (
+  state = INIT_STATE,
+  action: Action
+) => {
   switch (action.type) {
     case GlobalActionType.SetOpenLoginModalTrue:
       return { ...state, OpenLoginModal: true };
@@ -24,8 +26,8 @@ GlobalStateInterface,
       return { ...state, authenticated: true };
     case GlobalActionType.SetAuthenticatedFalse:
       return { ...state, authenticated: false };
-      case GlobalActionType.UserLoggedIn:
-        return { ...state, authenticated: true, OpenLoginModal: false };
+    case GlobalActionType.DoneLoading:
+      return { ...state, loading: false };
     default:
       return state;
   }
