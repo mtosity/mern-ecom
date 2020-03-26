7879629878d3d69ff17be68ad71abe6192321cd2
minhtamos <tam310599@gmail.com>
2020-03-26 19:22:25 +0700

    categories reducer

---
+++ client/src/Reducers/CategoriesReducer.tsx
@@ -0,0 +1,29 @@
+import { Reducer, Action } from "redux";
+import { CategoriesActionType } from "../Actions";
+import { CategoryType } from "../DataType";
+
+export interface CategoriesStateInterface extends Array<CategoryType> {}
+
+const INIT_STATE: CategoriesStateInterface = [{
+  name: "",
+  gender: "",
+  id: "",
+  createdAt: "",
+  updatedAt: ""
+}];
+
+interface CategoriesActionInterface extends Action {
+  payload: CategoriesStateInterface;
+}
+
+export const CategoriesReducer: Reducer<
+  CategoriesStateInterface,
+  CategoriesActionInterface
+> = (state: CategoriesStateInterface = INIT_STATE, action: CategoriesActionInterface) => {
+  switch (action.type) {
+    case CategoriesActionType.AddCategory:
+      return action.payload;
+    default:
+      return state;
+  }
+};
