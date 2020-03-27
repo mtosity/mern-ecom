9ab1e0bead1c94a13fc6052f7df052bff5ddfec4
minhtamos <tam310599@gmail.com>
2020-03-27 11:13:42 +0700

    select category from product cate

--- client/src/components/ProductCategories/SelectCategories.tsx
+++ client/src/components/ProductCategories/SelectCategories.tsx
@@ -6,30 +6,28 @@ import { ApplicationState } from "../../Reducers/CombinedReducers";
 import { CategoryType } from "../../DataType";
 import { useLocation } from "react-router-dom";
 
-export const SelectCategories = () => {
-  let location = useLocation();
-  const categories = useSelector<ApplicationState, Array<CategoryType>>(
-    state => state.CategoriesReducer
-  );
-  const loading = useSelector<ApplicationState, boolean>(
-    state => state.GlobalReducer.loading
-  );
-  const menCate = categories.filter(c => c.gender === "male");
-  const womenCate = categories.filter(c => c.gender === "female");
-  const [selectedCate, selectCate] = useState("");
-  useEffect(() => {
-    if (!loading) {
-      const catePath = location.pathname.split("/")[2];
-      const cate = categories.filter(c => c.name === catePath);
-      if (cate.length === 1) {
-        selectCate(cate[0].name);
-      }
-    }
-  }, [loading, location]);
+interface props {
+  loading: boolean;
+  womenCate: Array<CategoryType>;
+  menCate: Array<CategoryType>;
+  selectedCate: string;
+  selectCate: Function;
+}
+
+export const SelectCategories = ({
+  loading,
+  womenCate,
+  menCate,
+  selectedCate,
+  selectCate
+}: props) => {
   return loading ? (
     <div>Loading categories</div>
   ) : (
-    <div className="w-full flex p-16 md:flex-col" style={{ backgroundColor: "#F5F5F5" }}>
+    <div
+      className="w-full flex p-16 md:flex-col"
+      style={{ backgroundColor: "#F5F5F5" }}
+    >
       <div className="flex flex-col">
         <CategoryGenderTitle title="WOMEN" />
         {womenCate.map(cate => {
--- client/src/screen/ProductCategories.tsx
+++ client/src/screen/ProductCategories.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SelectCategories } from "../components/ProductCategories/SelectCategories";
+import { useSelector } from "react-redux";
+import { ApplicationState } from "../Reducers/CombinedReducers";
+import { useLocation } from "react-router-dom";
+import { CategoryType } from "../DataType";
 
 export const ProductCategories = () => {
+  let location = useLocation();
+  const categories = useSelector<ApplicationState, Array<CategoryType>>(
+    state => state.CategoriesReducer
+  );
+  const loading = useSelector<ApplicationState, boolean>(
+    state => state.GlobalReducer.loading
+  );
+  const menCate = categories.filter(c => c.gender === "male");
+  const womenCate = categories.filter(c => c.gender === "female");
+  const [selectedCate, selectCate] = useState("");
+  useEffect(() => {
+    if (!loading) {
+      const catePath = location.pathname.split("/")[2];
+      const cate = categories.filter(c => c.name === catePath);
+      if (cate.length === 1) {
+        selectCate(cate[0].name);
+      }
+    }
+  }, [loading, location]);
   return (
-    <div style={{}}>
-      <SelectCategories />
+    <div>
+      <SelectCategories 
+        menCate={menCate}
+        womenCate={womenCate}
+        selectedCate={selectedCate}
+        selectCate={selectCate}
+        loading={loading}
+      />
     </div>
   );
 };
