f8f489a6e3130a0dfb98b180cac5405f02178f89
minhtamos <tam310599@gmail.com>
2020-03-28 11:24:41 +0700

    product props add in

--- client/src/components/ProductCard/ProductCard.tsx
+++ client/src/components/ProductCard/ProductCard.tsx
@@ -3,33 +3,40 @@ import { PopupIcon } from "./PopupIcon";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { faHeart, faEye } from "@fortawesome/free-regular-svg-icons";
 import { ProductStars } from "../ProductDetail/ProductStars";
-export const ProductCard = () => {
+import { ProductType } from "../../DataType";
+import { Link } from "react-router-dom";
+
+interface props {
+  product: ProductType;
+}
+
+export const ProductCard = ({ product }: props) => {
   return (
-    <div className="flex justify-center items-center">
-      <div className="flex flex-col bg-white shadow-md mx-2 hover:shadow-2xl img-hover-darker relative group w-64">
-        <img
-          className=" object-cover w-64 h-64"
-          src="https://preview.uideck.com/items/shopr-theme/assets/img/products/img-08.jpg"
-          alt=""
-        />
+    <div>
+      <div
+        className="flex flex-col bg-white shadow-md mx-2 hover:shadow-2xl img-hover-darker relative group"
+        style={{ width: "250px" }}
+      >
+        <img className=" object-cover" src={product.image} alt=""  style={{width: "250px", height: "380px"}}/>
         <div
-          className="absolute top-0 left-0 w-full h-64 flex justify-center items-center
+          className="absolute top-0 left-0 w-full flex justify-center items-center
       -translate-y-5 group-hover:translate-y-0 transform duration-500 ease-in-out"
+      style={{height: "380px"}}
         >
           <PopupIcon icon={faCartPlus} />
           <PopupIcon icon={faHeart} />
           <PopupIcon icon={faEye} />
         </div>
-        <div className="w-56 px-1 py-2">
-          <a
-            href="#"
-            className="text-lg font-semibold inline-block ml-4 py-1 hover:text-red-700"
+        <div className="w-56 px-1 py-2 h-24 flex flex-col justify-around">
+          <Link
+            to={`/detail/${product.id}`}
+            className=" font-semibold inline-block ml-4 py-1 hover:text-red-700"
           >
-            Qui Ratione Volup
-          </a>
+            {product.title}
+          </Link>
           <div className="flex justify-between">
-            <p className="font-semibold ml-4">$49.00</p>
-            <ProductStars stars={4.5} onlyStars/>
+            <p className="font-semibold ml-4">${product.salePrice}</p>
+            <ProductStars stars={4.5} onlyStars />
           </div>
         </div>
       </div>
