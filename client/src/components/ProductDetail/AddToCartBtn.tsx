b1cd5b91e26c5f9554b287a7df3a038da3d2117a
minhtamos <tam310599@gmail.com>
2020-03-29 08:58:09 +0700

    add to cart have onlicl function

--- client/src/components/ProductDetail/AddToCartBtn.tsx
+++ client/src/components/ProductDetail/AddToCartBtn.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export const AddToCartBtn = () => {
+interface props {
+  onClick: Function;
+}
+
+export const AddToCartBtn = ({onClick}: props) => {
   return (
     <button className="flex justify-between font-bold items-center jo-font focus:outline-none px-6 py-3 
-    bg-red-700 hover:bg-red-500 text-white shadow-lg transform duration-500 ml-4 items-center
-    ">
+    bg-red-700 hover:bg-red-500 text-white shadow-lg transform duration-500 ml-4 items-center"
+    onClick={() => onClick()}
+    >
       <FontAwesomeIcon icon={faCartPlus}/>
       <p className="ml-4">ADD TO CART</p>
     </button>
