bb2c9f6cafe43f14b423cc13c3d2ef87e1d0c1cf
minhtamos <tam310599@gmail.com>
2020-04-03 13:33:45 +0700

    total price

--- client/src/components/TopInfo/CartDropDown.tsx
+++ client/src/components/TopInfo/CartDropDown.tsx
@@ -6,7 +6,6 @@ import { CartStateInterface } from "../../Reducers/CartReducer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 import { CartActionType } from "../../Actions";
-import useOutsideClick from "../../utils/useOutsideClick";
 
 interface props {
   show: boolean;
@@ -17,6 +16,11 @@ export const CartDropDown = ({ show }: props) => {
   const cart = useSelector<ApplicationState, CartStateInterface>(
     state => state.CartReducer
   );
+  const prices = cart.map(product => product.salePrice * product.quantity);
+  const total =
+    prices.length === 0
+      ? 0
+      : prices.reduce((accumulator, current) => accumulator + current).toFixed(2);
 
   const dispatcher = useDispatch();
   return (
@@ -32,21 +36,20 @@ export const CartDropDown = ({ show }: props) => {
         style={{
           borderRight: "15px solid transparent",
           borderLeft: "15px solid transparent",
-          borderBottom: "15px solid white"
+          borderBottom: "15px solid red"
         }}
       ></div>
-      <div className="bg-white cursor-auto shadow-lg" ref={ref}>
+      <div
+        className="bg-white cursor-auto shadow-lg border border-red-600"
+        ref={ref}
+      >
         {cart.length === 0 ? (
-          <div
-            className="w-64 h-20 pt-4"
-          >
-            No item
-          </div>
+          <div className="w-64 h-20 pt-4">No item</div>
         ) : (
           cart.map(product => {
             return (
               <div
-                className="p-2 flex items-center"
+                className="p-2 flex"
                 key={`cart${product.id}`}
                 style={{ width: "350px" }}
               >
@@ -55,16 +58,19 @@ export const CartDropDown = ({ show }: props) => {
                   alt=""
                   className="h-32 w-32 object-contain"
                 />
-                <div className="h-full mt-2 overflow-hidden ml-2">
+                <div className="h-full overflow-hidden ml-2 flex flex-col items-start justify-center">
                   <p className="font-bold text-lg">{product.title}</p>
                   <div className="flex text-gray-600 text-sm">
                     <p>Size:</p>
                     <p className="font-bold ml-1">{product.size}</p>
                     <p className="ml-2">Color:</p>
-                    <p className="ml-1" style={{ color: product.color }}>{product.color}</p>
+                    <p className="ml-1" style={{ color: product.color }}>
+                      {product.color}
+                    </p>
                   </div>
-                  <p className="text-gray-600 text-sm">
-                    Quantity: <span className="font-bold">{product.quantity}</span>
+                  <p className="text-gray-600 text-sm bg">
+                    Quantity:{" "}
+                    <span className="font-bold">{product.quantity}</span>
                   </p>
                   <p className="text-red-600 font-bold text-lg">
                     ${product.salePrice.toFixed(2)}
@@ -72,7 +78,7 @@ export const CartDropDown = ({ show }: props) => {
                   <FontAwesomeIcon
                     icon={faTrashAlt}
                     color="red"
-                    className="cursor-pointer mt-4"
+                    className="cursor-pointer mt-2"
                     onClick={() =>
                       dispatcher({
                         type: CartActionType.DeleteProduct,
@@ -86,6 +92,14 @@ export const CartDropDown = ({ show }: props) => {
             );
           })
         )}
+        <div className="flex">
+          <div className="flex-1 justify-center items-center bg-red-600 text-white font-bold p-2">
+            <p>CHECK OUT</p>
+          </div>
+          <div className="flex-1 justify-center items-center text-red-600 font-bold p-2 border-t border-red text-sm">
+            <p>Total: ${total}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
