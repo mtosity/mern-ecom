ffe084a5b29ac69dc1647c8de5d4465143e07bb3
minhtamos <tam310599@gmail.com>
2020-04-06 23:15:53 +0700

    purgecss only in production

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -3,13 +3,13 @@ module.exports = {
     plugins: [
         tailwindcss('./tailwind.config.js'),
         require('autoprefixer'),
-        // require('@fullhuman/postcss-purgecss')({
-        //     content: [
-        //       './src/**/*.tsx',
-        //       './public/index.html'
-        //     ],
-        //     css: ['./src/**/*.css'],
-        //     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-        // })
+        process.env.NODE_ENV==="production" && require('@fullhuman/postcss-purgecss')({
+            content: [
+              './src/**/*.tsx',
+              './public/index.html'
+            ],
+            css: ['./src/**/*.css'],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        })
     ],
 };
