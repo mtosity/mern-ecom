424b3b15fa58faf3fa093a369052f652f8cee17e
minhtamos <tam310599@gmail.com>
2020-04-08 23:51:44 +0700

    fixed untracked files

--- client/src/screen/FOF.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const FOF = () => {
-    return (
-        <div className="flex w-full justify-center">
-            <img src="https://www.elsetge.cat/myimg/f/99-992071_404-page-error-with-funny-figure-skull.jpg" alt="" className="block"/>
-        </div>
-    )
-}
